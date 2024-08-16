0472f0f58faf2dbc101c0e75013b81d9a3dc8e59
Prytula Olexandr <liga5872@gmail.com>
2024-08-16 11:58:47 +0300

    add some fix of slider footer

--- src/components/FullWidthSlider/FullWidthSlider.tsx
+++ src/components/FullWidthSlider/FullWidthSlider.tsx
@@ -37,6 +37,20 @@ const slides: Slidse[] = [
       "As the most powerful launch system ever developed, Starship will be able to carry up to 100 people on long-duration, interplanetary flights. Starship will also help enable satellite delivery, the development of a Moon base, and point-to-point transport here on Earth.",
     imageUrl: "./img/DRAGON_DRAGON+F9.jpg",
   },
+  {
+    id: 4,
+    title: "Starship capabilities",
+    description:
+      "As the most powerful launch system ever developed, Starship will be able to carry up to 100 people on long-duration, interplanetary flights. Starship will also help enable satellite delivery, the development of a Moon base, and point-to-point transport here on Earth.",
+    imageUrl: "./img/DRAGON_DRAGON+F9.jpg",
+  },
+  {
+    id: 5,
+    title: "Starship capabilities",
+    description:
+      "As the most powerful launch system ever developed, Starship will be able to carry up to 100 people on long-duration, interplanetary flights. Starship will also help enable satellite delivery, the development of a Moon base, and point-to-point transport here on Earth.",
+    imageUrl: "./img/DRAGON_DRAGON+F9.jpg",
+  },
 ];
 
 const FullWidthSlider: React.FC = () => {
@@ -68,19 +82,25 @@ const FullWidthSlider: React.FC = () => {
               className={styles.titleWrapper}
               style={{ backgroundImage: `url(${slide.imageUrl})` }}
             >
-              <div className={styles.content}>
-                <div className={styles.title}>{slide.title}</div>
-                <p className={styles.subTitle}>{slide.description}</p>
+              <div className={styles.swiperContainer}>
+                <div className={styles.contentWrapper}>
+                  <div className={styles.content}>
+                    <div className={styles.title}>{slide.title}</div>
+                    <p className={styles.subTitle}>{slide.description}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.swiperContainer}>
         <div className={styles.navButtons}>
           <div className={styles.swiperButtonPrev}></div>
           <div className={styles.swiperPagination}></div>
           <div className={styles.swiperButtonNext}></div>
         </div>
+      </div>
     </div>
   );
 };
